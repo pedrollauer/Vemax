c016658821ff79e47108774f02b7ec2db1d02d14
pedrollauer <85714397+pedrollauer@users.noreply.github.com>
2023-08-26 14:01:54 -0300

    Tela principal
    Criado o layout da tela principal de operação.

---
+++ app/src/main/java/com/pedro/vemax/Operacao.java
@@ -0,0 +1,14 @@
+package com.pedro.vemax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Operacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacao);
+    }
+}
